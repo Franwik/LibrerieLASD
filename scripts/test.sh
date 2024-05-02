d1be39494e3593c9ec7d2bcb3cbe4cc6caa3c8a9
Franwik <franwik_@outlook.com>
2024-05-02 14:50:18 +0200

    fix[CI]: changed test.sh to run only exercise 1

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,15 +1,31 @@
- find ./.. -name makefile | while read makefile; do
-            dir=$(dirname "$makefile")
-            make -C "$dir"
-            $dir/main
-            result=$?
-            output="Test terminated with $result error"
-            if [ "$result" -ne 1 ]; then
-              output+="s"
-            fi
-            echo $output
-            if [ "$result" -ne 0 ]; then
-              echo "Test failed! Please check the code before pushing again."
-              exit 1
-            fi
-          done
+
+# Make and Execute exercise 1
+
+mske -C exercise1
+exercise1/main
+result=$?
+output="Test terminated with $result error"
+if [ "$result" -ne 1 ]; then
+  output+="s"
+fi
+echo $output
+if [ "$result" -ne 0 ]; then
+  echo "Test failed! Please check the code before pushing again."
+  exit 1
+fi
+
+#  find ./../exercise1 -name makefile | while read makefile; do
+#             dir=$(dirname "$makefile")
+#             make -C "$dir"
+#             $dir/main
+#             result=$?
+#             output="Test terminated with $result error"
+#             if [ "$result" -ne 1 ]; then
+#               output+="s"
+#             fi
+#             echo $output
+#             if [ "$result" -ne 0 ]; then
+#               echo "Test failed! Please check the code before pushing again."
+#               exit 1
+#             fi
+#           done
