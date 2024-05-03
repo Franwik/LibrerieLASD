ef4efa244ecdb48b25085d8247625dd29c6d9b8c
Franwik <franwik_@outlook.com>
2024-05-03 12:00:02 +0200

    fix[EX2]: commented code to run ex1 tests

---
+++ exercise2/main.cpp
@@ -0,0 +1,16 @@
+#include "zlasdtest/test.hpp"
+
+#include "zmytest/test.hpp"
+
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+/* ************************************************************************** */
+
+int main() {
+  lasdtest();
+  // mytest();
+  return 0;
+}
